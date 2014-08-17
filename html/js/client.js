76ff62e9a078e896ad36b1b734df8eb9627381ab
Chris Brame <polonel@gmail.com>
2014-08-17 03:16:18 -0400

    Messages

--- html/js/client.js
+++ html/js/client.js
@@ -1,4 +1,29 @@
-$(document).foundation();
-$(document).ready(function() {
+$j = jQuery.noConflict();
 
-})
+$j(document).foundation();
+$j(document).ready(function() {
+
+
+    $j('.message-items > li').click(function() {
+        var a = $j(this).find('a');
+        if (a.length) {
+            var href = a.attr('href');
+            if (href.length)
+                window.location.href = href;
+        }
+    });
+
+    $j(window).resize(function() {
+        resizeFullHeight();
+    });
+    $j(window).resize();
+});
+
+
+function resizeFullHeight() {
+    var ele = $j('.full-height');
+    $j.each(ele, function() {
+        var h = $j(window).height();
+        $j(this).height(h - $j(this).offset().top);
+    });
+}
