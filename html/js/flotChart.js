c43426bf17f9c27ad1b1aef248f93413229d7518
Chris Brame <polonel@gmail.com>
2014-09-16 14:03:56 -0400

    Dual Chart on Dashboard Calendar

--- html/js/flotChart.js
+++ html/js/flotChart.js
@@ -5,9 +5,15 @@ var data1 = [
     [gd(2014, 4, 1), 1050], [gd(2014, 5, 1), 400], [gd(2014, 6, 1), 560], [gd(2014, 7, 1), 620],
     [gd(2014, 8, 1), 1175], [gd(2014, 9, 1), 850], [gd(2014, 10, 1), 1495], [gd(2014, 11, 2), 125]
 ];
+var data2 = [
+    [gd(2014, 0, 1), 580], [gd(2014, 1, 1), 231], [gd(2014, 2, 1), 444], [gd(2014, 3, 1), 1123],
+    [gd(2014, 4, 1), 111], [gd(2014, 5, 1), 640], [gd(2014, 6, 1), 235], [gd(2014, 7, 1), 100],
+    [gd(2014, 8, 1), 240], [gd(2014, 9, 1), 600], [gd(2014, 10, 1), 1058], [gd(2014, 11, 2), 290]
+];
 
 var dataset = [
-    { data: data1}
+    { data: data1, label: "New"},
+    { data: data2, label: "Closed"}
 ];
 
 function gd(year, month, day) {
@@ -55,6 +61,7 @@ var options = {
     }
     ],
     legend: {
+        show: false,
         noColumns: 0,
         labelBoxBorderColor: "#000000",
         position: "nw"
@@ -65,7 +72,22 @@ var options = {
         borderColor: "#633200"
         //backgroundColor: { colors: ["#ffffff", "#EDF5FF"] }
     },
-    colors: ["#e74c3c"]
+    tooltip: true,
+    tooltipOpts: {
+        content: function(label, xVal, yVal, item) {
+            var color = "#e74c3c";
+            if (label === 'Closed')
+                color = "#3498db";
+            return "<span>%x</span><span style='color: " + color + ";'>%y <span style='font-weight: 300;'>%s</span></span>";
+        },
+        monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        xDateFormat: "%b, %Y",
+        shifts: {
+            x: -75,
+            y: -75
+        }
+    },
+    colors: ["#e74c3c", "#2fb150"]
 };
 
 $j(document).ready(function() {
