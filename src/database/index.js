5a6b99ffde0023239b89dbae350180bb3319fd59
Chris Brame <polonel@gmail.com>
2014-11-24 02:10:04 -0500

    typo

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
 
 var db = {};
 //var CONNECTION_URI = 'mongodb://trudesk:#TruDesk$@127.0.0.1/trudesk';
-var CONNECTION_URI = 'mongodb://truedesk:#TruDesk$@dogen.mongohq.com:10094/app31908899/trudesk';
+var CONNECTION_URI = 'mongodb://truedesk:#TruDesk$@dogen.mongohq.com:10094/app31908899';
 
 module.exports.init = function(callback) {
     mongoose.connect(CONNECTION_URI, {auto_reconnect: true}, function(e) {
