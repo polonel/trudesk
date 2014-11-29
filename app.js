aa706155335ddd958b314400c6fd63851a12be6d
Chris Brame <polonel@gmail.com>
2014-11-29 14:39:50 -0500

    Disabled NodeTime

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ winston.add(winston.transports.File, {
 });
 
 function start() {
-    if (process.env.NODETIME_ACCOUNT_KEY) {
-        require('nodetime').profile({
-            accountKey: process.env.NODETIME_ACCOUNT_KEY,
-            appName: 'TruDesk'
-        });
-    }
+//    if (process.env.NODETIME_ACCOUNT_KEY) {
+//        require('nodetime').profile({
+//            accountKey: process.env.NODETIME_ACCOUNT_KEY,
+//            appName: 'TruDesk'
+//        });
+//    }
 
     winston.info('Time: ' + new Date());
 
