621c0f8d7b67d1c87cd1a28d9b33efc409d21368
Chris Brame <polonel@gmail.com>
2014-12-16 03:54:14 -0500

    Updated

--- src/mailer/index.js
+++ src/mailer/index.js
@@ -1,17 +1,17 @@
 var nodeMailer = require('nodemailer');
 var transporter = nodeMailer.createTransport({
-    host:   'granvillecounty.org',
+    host:   'mailer.com',
     port:   25,
     auth: {
-        user: 'chris.brame',
-        pass: 'A03251969a'
+        user: 'chris',
+        pass: '1234'
     }
 });
 
 
 module.exports = function() {
     transporter.sendMail({
-        from:   'trudesk@granvillecounty.org',
+        from:   'trudesk@123.org',
         to:     'polonel@gmail.com',
         subject:    'Testing TruDesk Email',
         text:       'Hello this is from TruDesk'
