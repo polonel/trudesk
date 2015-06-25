0a578fdb37b5c2934733999db39d494037612d3d
Chris Brame <polonel@gmail.com>
2015-06-25 17:25:26 -0400

    MailCheck Update

--- src/mailer/mailCheck.js
+++ src/mailer/mailCheck.js
@@ -20,20 +20,28 @@ var MailParser  = require('mailparser').MailParser;
 var winston     = require('winston');
 var nconf       = require('nconf');
 
-var MAILER_ENABLED = nconf.get('mailer:enable');
-var POOL_INTERVAL = nconf.get('mailer:polling') ? nconf.get('mailer:polling') : 3600000; //1hour
+var userSchema  = require('../models/user');
+var Ticket      = require('../models/ticket');
+
+var MAILER_ENABLED = nconf.get('mailer:check:enable');
+var MAILERCHECK_USER = nconf.get('mailer:check:user') ? nconf.get('mailer:check:user') : MAILER_ENABLED = false;
+var MAILERCHECK_PASS = nconf.get('mailer:check:pass') ? nconf.get('mailer:check:pass') : MAILER_ENABLED = false;
+var MAILERCHECK_HOST = nconf.get('mailer:check:host') ? nconf.get('mailer:check:host') : MAILER_ENABLED = false;
+var POOL_INTERVAL = nconf.get('mailer:check:polling') ? nconf.get('mailer:check:polling') : 3600000; //1hour
 
 var mailCheck = {};
 mailCheck.Imap = new Imap({
-    user: 'trudesk',
-    password: 'Granville789',
-    host: 'mail.granvillecounty.org',
+    user: MAILERCHECK_USER,
+    password: MAILERCHECK_PASS,
+    host: MAILERCHECK_HOST,
     port: 143
 });
 
 mailCheck.inbox = [];
 
 mailCheck.init = function() {
+    if (!MAILER_ENABLED) return true;
+
     setInterval(function() {
         mailCheck.fetchMail();
     }, 10000);
@@ -42,6 +50,7 @@ mailCheck.init = function() {
 mailCheck.fetchMail = function() {
     mailCheck.Imap.connect();
     mailCheck.Imap.once('ready', function() {
+        var messages = [];
         openInbox(function(err, box) {
             if (err) throw err;
             async.waterfall([
@@ -54,10 +63,14 @@ mailCheck.fetchMail = function() {
                         mailCheck.Imap.end();
                         return next();
                     }
+
+                    var message = {};
+
                     var f = mailCheck.Imap.fetch(results, {
                         markSeen: true,
                         bodies: ['HEADER.FIELDS (FROM TO SUBJECT)','TEXT']
                     });
+
                     f.on('message', function(msg, seqno) {
                         msg.on('body', function(stream, info) {
                             var buffer = '', count = 0;
@@ -68,16 +81,27 @@ mailCheck.fetchMail = function() {
                             stream.once('end', function() {
                                 if (info.which !== 'TEXT') {
                                     var header = Imap.parseHeader(buffer);
-                                    console.log(header.subject);
+                                    if (_.isArray(header.subject) && _.size(header.subject) > 0)
+                                        message.subject = header.subject[0];
+                                    if (_.isArray(header.from) && _.size(header.from) > 0)
+                                        message.from = parseEmail(header.from[0]);
                                 } else
-                                    console.log('Message Buffer: ' + buffer);
+                                    message.body = buffer;
                             });
                         });
                         msg.once('attributes', function(attrs) {
                             //console.log('Attributes: %s', inspect(attrs, false, 8));
                         });
                         msg.once('end', function() {
+                            userSchema.getUserByEmail(message.from, function(err, user) {
+                                if (!err && user) {
+                                    message.owner = user;
 
+                                    messages.push(message);
+
+                                    console.log(messages);
+                                }
+                            });
                         });
                     });
                     f.once('error', function(err) {
@@ -90,11 +114,28 @@ mailCheck.fetchMail = function() {
                     });
                 }
             ], function(err) {
+                if (err) winston.warn(err);
                 mailCheck.Imap.end();
             });
         });
     });
 };
+
+function parseEmail(email) {
+    var strArray1 = email.split('<');
+    var strArray2 = strArray1[1].split('>');
+    var actualEmail = strArray2[0];
+
+    if (validateEmail(actualEmail))
+        return actualEmail;
+    else
+        return '';
+}
+
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
 
 function openInbox(cb) {
     mailCheck.Imap.openBox('INBOX', false, cb);
