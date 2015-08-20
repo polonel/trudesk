778408a93e6cd41cdc7422ec9bacb9975fcaa01c
Chris Brame <polonel@gmail.com>
2015-08-20 02:50:27 -0400

    Test Deploy

--- gruntfile.js
+++ gruntfile.js
@@ -148,6 +148,77 @@ module.exports = function(grunt) {
                     //configure : "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json"
                 }
             }
+        },
+
+        requirejs: {
+            compile: {
+                options: {
+                    appDir: 'src/public/js',
+                    baseUrl: './',
+                    mainConfigFile: 'src/public/js/config.js',
+                    dir: 'public/js',
+                    removeCombined: true,
+                    preserveLicenseComments: false,
+                    kipDirOptimize: false,
+                    optimize: 'uglify2',
+                    uglify2: {
+                        mangle: false
+                    },
+                    modules: [
+                        {
+                            name: 'trudesk.min',
+                            create: true,
+                            include: [
+                                'jquery',
+                                'jquery_scrollTo',
+                                'foundation',
+                                'angular',
+                                'angularRoute',
+                                'modernizr',
+                                'fastclick',
+                                'placeholder',
+                                'nicescroll',
+                                'underscore',
+                                'history',
+
+                                'angularjs/main',
+                                'angularjs/controllers',
+                                'app',
+
+                                'modules/ajaxify',
+                                'modules/ajaximgupload',
+                                'modules/attachmentUpload',
+
+                                'pages/accounts',
+                                'pages/dashboard',
+                                'pages/editaccount',
+                                'pages/groups',
+                                'pages/messages',
+                                'pages/reports',
+                                'pages/singleTicket',
+                                'pages/tickets'
+                            ],
+                            shim: {
+                                angular: {
+                                    exports: 'angular'
+                                }
+                            }
+                        }
+                        //{
+                        //    name: 'page-accounts',
+                        //    create: true,
+                        //    include: [ 'pages/accounts' ],
+                        //    exclude: [ 'trudesk.min' ]
+                        //}
+                    ],
+                    paths: {
+                        foundation: 'empty:',
+                        angular: 'empty:',
+                        angularRoute: 'empty:'
+                    },
+                    keepBuildDir: true
+                }
+            }
         }
     });
 
@@ -155,5 +226,6 @@ module.exports = function(grunt) {
     grunt.registerTask('builddocs', ['jsdoc', 'apidoc']);
     grunt.registerTask('watchdocs', ['parallel:docs']);
     grunt.registerTask('server', 'launch webserver and watch tasks', ['parallel:web']);
+    grunt.registerTask('build', ['buildcss', 'builddocs', 'requirejs']);
     grunt.registerTask('default', ['server']);
 };
