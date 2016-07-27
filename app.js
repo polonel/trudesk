dbc2a7b59831dd0cab18332a387b7670d1f43dc3
Chris Brame <polonel@gmail.com>
2016-07-27 02:25:06 -0400

    Fixed issue with cache not loading

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ function dbCallback(err, db) {
                         var fork = require('child_process').fork;
                         var n = fork(path.join(__dirname, '/src/cache/index.js'), { env: {
                             FORK: 1, NODE_ENV: global.env,
-                            MONGODB_HOSTNAME: process.env.MONGODB_HOSTNAME,
+                            MONGODB_DATABASE_NAME: process.env.MONGODB_DATABASE_NAME,
                             MONGODB_PORT_27017_TCP_ADDR: process.env.MONGODB_PORT_27017_TCP_ADDR
                         } } );
 
