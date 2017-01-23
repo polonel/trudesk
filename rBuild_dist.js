cb1dabd038eec1c3afe327295eda7d0e1d4c9da6
Chris Brame <polonel@gmail.com>
2017-01-23 16:57:55 -0500

    Add dist build to grunt file

---
+++ rBuild_dist.js
@@ -0,0 +1,79 @@
+({
+    appDir: 'src/public/js',
+    baseUrl: './',
+    mainConfigFile: 'src/public/js/config.js',
+    dir: 'public/js',
+    removeCombined: true,
+    preserveLicenseComments: false,
+    kipDirOptimize: false,
+    optimize: 'uglify2',
+    //optimize: 'none',
+    uglify2: {
+    mangle: false
+},
+modules: [
+    {
+        name: 'trudesk.min',
+        create: true,
+        include: [
+            'jquery',
+            'jquery_scrollTo',
+            'jquery_custom',
+            'uikit',
+            'angular',
+            'angularRoute',
+            'angularCookies',
+            'angularSanitize',
+            'modernizr',
+            'fastclick',
+            'placeholder',
+            'nicescroll',
+            'underscore',
+            'history',
+            'd3',
+            'metricsgraphics',
+            'd3pie',
+            'peity',
+            'countup',
+            'selectize',
+            'waves',
+            'formvalidator',
+            'snackbar',
+
+            '../../src/permissions/roles',
+
+            'angularjs/main',
+            'angularjs/controllers',
+            'app',
+
+            'modules/ajaxify',
+            'modules/ajaximgupload',
+            'modules/attachmentUpload',
+
+            'pages/accounts',
+            'pages/dashboard',
+            'pages/editaccount',
+            'pages/groups',
+            'pages/messages',
+            'pages/reports',
+            'pages/singleTicket',
+            'pages/tickets',
+            'pages/notices',
+            'pages/settings'
+        ],
+        shim: {
+            angular: {
+                exports: 'angular'
+            }
+        }
+    }
+],
+    paths: {
+        //foundation: 'empty:',
+        angular: 'empty:',
+        angularRoute: 'empty:',
+        angularCookies: 'empty:',
+        angularSanitize: 'empty:'
+},
+keepBuildDir: true
+})
