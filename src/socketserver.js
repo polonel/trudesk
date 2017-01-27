cac7c99e6d3d716f839ce763cd2c017fddcef107
Chris Brame <polonel@gmail.com>
2017-01-26 21:01:42 -0500

    Removed 5sec polling (Hopefully fixes the JANK issue)

--- src/socketserver.js
+++ src/socketserver.js
@@ -89,14 +89,16 @@ module.exports = function(ws) {
     io.sockets.on('connection', function(socket) {
         var totalOnline = _.size(usersOnline);
 
-        //TODO: This is a JANK lag that needs to be removed and optimized!!!!
-        // setInterval(function() {
-        //     updateConversationsNotifications();
-        //     updateNotifications();
-        //     var sortedUserList = __.object(__.sortBy(__.pairs(usersOnline), function(o) { return o[0]}));
-        //     utils.sendToSelf(socket, 'updateUsers', sortedUserList);
-        //
-        // }, 5000);
+        setInterval(function() {
+            updateConversationsNotifications();
+            updateNotifications();
+
+            //TODO: This is a JANK lag that needs to be removed and optimized!!!!
+            // Running Test to see if this is actually needed
+            //var sortedUserList = __.object(__.sortBy(__.pairs(usersOnline), function(o) { return o[0]}));
+            //utils.sendToSelf(socket, 'updateUsers', sortedUserList);
+
+        }, 5000);
 
         function updateConversationsNotifications() {
             var userId = socket.request.user._id;
