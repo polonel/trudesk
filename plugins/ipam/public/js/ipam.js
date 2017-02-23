d6155980228ed2bdc6df26912a7aab21e9c5332f
Chris Brame <polonel@gmail.com>
2017-02-23 02:07:09 -0500

    Removed Ignored Tracked files

--- plugins/ipam/public/js/ipam.js
+++
@@ -1,6 +0,0 @@
-window.truRequire(['jquery'], function(loadedModules) {
-    var $ = loadedModules[0];
-    setTimeout(function() {
-        $('.ipam-test').css({color: 'green'});
-    }, 4000);
-});
