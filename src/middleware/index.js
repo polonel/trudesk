e89f2f845bfa2de1e828223b7a90e1ba916b8f22
Chris Brame <polonel@gmail.com>
2019-02-06 18:20:40 -0500

    fix(images): profile images not displaying in new email templats

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -98,6 +98,7 @@ module.exports = function (app, db, callback) {
         app.use('/mobile', express.static(path.join(__dirname, '../../', 'mobile')))
 
         app.use('/assets', express.static(path.join(__dirname, '../../public/uploads/assets')))
+        app.use('/uploads/users', express.static(path.join(__dirname, '../../public/uploads/users')))
         app.use('/uploads', middleware.hasAuth, express.static(path.join(__dirname, '../../public/uploads')))
         app.use(
           '/backups',
