1522e6d355aaccbe4d8961ada6de444fe7963575
YAponchik <fraxium1999@yandex.ru>
2023-01-16 16:24:04 +0000

    Issue-9 коректировка 116

--- src/emitter/events.js
+++ src/emitter/events.js
@@ -146,6 +146,7 @@ const eventTicketCommentAdded = require('./events/event_ticket_comment_added')
     emitter.on('ticket:tcm:update', async function (data) {
       // Goes to client
       io.sockets.emit('$trudesk:client:tcm:update', data)
+      io.sockets.emit('$trudesk:client:tcm:update', data)
       //await eventTicketCommentAdded(ticket,comment,hostname)
     })
 
