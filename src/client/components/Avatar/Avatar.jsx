8449989c1c1457057e3eb556ba45a894126bef56
YAponchik <fraxium1999@yandex.ru>
2023-01-17 07:23:35 +0000

    Issue-9 коректировка 125

--- src/client/components/Avatar/Avatar.jsx
+++ src/client/components/Avatar/Avatar.jsx
@@ -77,7 +77,6 @@ class Avatar extends React.Component {
   }
 
   onMouseOver () {
-    console.log('Мышка двигается')
     if (this.overlayRef.current && this.overlayRef.current.classList.contains('uk-hidden')) {
       this.overlayRef.current.classList.remove('uk-hidden')
     }
