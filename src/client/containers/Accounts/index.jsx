e3b99c01d0ea85efa5554fe28c2e916447723068
Alexandder Novozhenin <novozhenina@gmail.com>
2023-02-07 14:24:51 +0000

    Isuue-11 3

--- src/client/containers/Accounts/index.jsx
+++ src/client/containers/Accounts/index.jsx
@@ -12,126 +12,128 @@
  *  Copyright (c) 2014-2019. All rights reserved.
  */
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { observer } from 'mobx-react'
-import { makeObservable, observable } from 'mobx'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { observer } from 'mobx-react';
+import { makeObservable, observable } from 'mobx';
 
-import { showModal } from 'actions/common'
-import { fetchAccounts, deleteAccount, enableAccount, unloadAccounts } from 'actions/accounts'
+import { showModal } from 'actions/common';
+import { fetchAccounts, deleteAccount, enableAccount, unloadAccounts } from 'actions/accounts';
 
-import Avatar from 'components/Avatar/Avatar'
-import TruCard from 'components/TruCard'
-import PageTitle from 'components/PageTitle'
-import Grid from 'components/Grid'
-import GridItem from 'components/Grid/GridItem'
-import PageContent from 'components/PageContent'
-import DropdownItem from 'components/Dropdown/DropdownItem'
-import DropdownTrigger from 'components/Dropdown/DropdownTrigger'
-import DropdownHeader from 'components/Dropdown/DropdownHeader'
-import Dropdown from 'components/Dropdown'
-import ButtonGroup from 'components/ButtonGroup'
-import Button from 'components/Button'
-import InfiniteScroll from 'react-infinite-scroller'
+import Avatar from 'components/Avatar/Avatar';
+import TruCard from 'components/TruCard';
+import PageTitle from 'components/PageTitle';
+import Grid from 'components/Grid';
+import GridItem from 'components/Grid/GridItem';
+import PageContent from 'components/PageContent';
+import DropdownItem from 'components/Dropdown/DropdownItem';
+import DropdownTrigger from 'components/Dropdown/DropdownTrigger';
+import DropdownHeader from 'components/Dropdown/DropdownHeader';
+import Dropdown from 'components/Dropdown';
+import ButtonGroup from 'components/ButtonGroup';
+import Button from 'components/Button';
+import InfiniteScroll from 'react-infinite-scroller';
 
-import helpers from 'lib/helpers'
+import helpers from 'lib/helpers';
 
 @observer
 class AccountsContainer extends React.Component {
-  @observable initialLoad = true
-  @observable hasMore = true
-  @observable pageStart = -1
+  @observable initialLoad = true;
+  @observable hasMore = true;
+  @observable pageStart = -1;
 
-  constructor (props) {
-    super(props)
-    makeObservable(this)
+  constructor(props) {
+    super(props);
+    makeObservable(this);
 
-    this.getUsersWithPage = this.getUsersWithPage.bind(this)
+    this.getUsersWithPage = this.getUsersWithPage.bind(this);
   }
 
-  componentDidMount () {
-    this.initialLoad = false
+  componentDidMount() {
+    this.initialLoad = false;
   }
 
-  componentDidUpdate () {
-    helpers.resizeFullHeight()
+  componentDidUpdate() {
+    helpers.resizeFullHeight();
   }
 
-  componentWillUnmount () {
-    this.props.unloadAccounts()
+  componentWillUnmount() {
+    this.props.unloadAccounts();
   }
 
-  onEditAccountClicked (e, user) {
-    e.preventDefault(e)
-    const canEditAccount = helpers.hasHierarchyOverRole(user.getIn(['role', '_id']))
+  onEditAccountClicked(e, user) {
+    e.preventDefault(e);
+    const canEditAccount = helpers.hasHierarchyOverRole(user.getIn(['role', '_id']));
     this.props.showModal('EDIT_ACCOUNT', {
       edit: canEditAccount,
       user: user.toJS(),
       roles: this.props.common.roles,
-      groups: this.props.common.groups
-    })
+      groups: this.props.common.groups,
+    });
   }
 
-  onDeleteAccountClicked (e, user) {
-    e.preventDefault()
-    this.props.deleteAccount({ username: user.get('username') })
+  onDeleteAccountClicked(e, user) {
+    e.preventDefault();
+    this.props.deleteAccount({ username: user.get('username') });
   }
 
-  onEnableAccountClicked (e, user) {
-    e.preventDefault()
-    this.props.enableAccount({ username: user.get('username') })
+  onEnableAccountClicked(e, user) {
+    e.preventDefault();
+    this.props.enableAccount({ username: user.get('username') });
   }
 
-  getUsersWithPage (page) {
-    this.hasMore = false
+  getUsersWithPage(page) {
+    this.hasMore = false;
     this.props.fetchAccounts({ page, limit: 25, type: this.props.view, showDeleted: true }).then(({ response }) => {
-      this.hasMore = response.count >= 25
-    })
+      this.hasMore = response.count >= 25;
+    });
   }
 
-  onSearchKeyUp (e) {
-    const keyCode = e.keyCode || e.which
-    const search = e.target.value
+  onSearchKeyUp(e) {
+    const keyCode = e.keyCode || e.which;
+    const search = e.target.value;
     if (keyCode === 13) {
       if (search.length > 2) {
         this.props.unloadAccounts().then(() => {
-          this.hasMore = false
+          this.hasMore = false;
           this.props.fetchAccounts({ limit: -1, search: search }).then(({ response }) => {
-            this.pageStart = -1
-            this.hasMore = response.count >= 25
-          })
-        })
+            this.pageStart = -1;
+            this.hasMore = response.count >= 25;
+          });
+        });
       } else if (search.length === 0) {
         this.props.unloadAccounts().then(() => {
-          this.pageStart = -1
-          this.getUsersWithPage(0)
-        })
+          this.pageStart = -1;
+          this.getUsersWithPage(0);
+        });
       }
     }
   }
 
-  render () {
+  render() {
     const items =
       this.props.accountsState.accounts &&
-      this.props.accountsState.accounts.map(user => {
-        const userImage = user.get('image') || 'defaultProfile.jpg'
-        let actionMenu = [<DropdownItem key={0} text={'Edit'} onClick={e => this.onEditAccountClicked(e, user)} />]
+      this.props.accountsState.accounts.map((user) => {
+        const userImage = user.get('image') || 'defaultProfile.jpg';
+        let actionMenu = [<DropdownItem key={0} text={'Edit'} onClick={(e) => this.onEditAccountClicked(e, user)} />];
         if (user.get('deleted'))
-          actionMenu.push(<DropdownItem key={2} text={'Enable'} onClick={e => this.onEnableAccountClicked(e, user)} />)
+          actionMenu.push(
+            <DropdownItem key={2} text={'Enable'} onClick={(e) => this.onEnableAccountClicked(e, user)} />
+          );
         else
           actionMenu.push(
             <DropdownItem
               key={1}
               text={'Delete'}
               extraClass={'uk-text-danger'}
-              onClick={e => this.onDeleteAccountClicked(e, user)}
+              onClick={(e) => this.onDeleteAccountClicked(e, user)}
             />
-          )
-        const isAdmin = user.getIn(['role', 'isAdmin']) || false
-        const isAgent = user.getIn(['role', 'isAgent']) || false
-        const customer = !isAdmin && !isAgent
-        const isDeleted = user.get('deleted') || false
+          );
+        const isAdmin = user.getIn(['role', 'isAdmin']) || false;
+        const isAgent = user.getIn(['role', 'isAgent']) || false;
+        const customer = !isAdmin && !isAgent;
+        const isDeleted = user.get('deleted') || false;
         return (
           <GridItem key={user.get('_id')} width={'1-5'} xLargeWidth={'1-6'} extraClass={'mb-25'}>
             <TruCard
@@ -144,7 +146,7 @@ class AccountsContainer extends React.Component {
               }
               header={
                 <div>
-                  <div className='account-image relative uk-display-inline-block'>
+                  <div className="account-image relative uk-display-inline-block">
                     <Avatar
                       size={82}
                       userId={user.get('_id')}
@@ -155,51 +157,51 @@ class AccountsContainer extends React.Component {
                       showLargerBubble={true}
                     />
                   </div>
-                  <h3 className='tru-card-head-text uk-text-center'>
+                  <h3 className="tru-card-head-text uk-text-center">
                     {user.get('fullname')}
-                    <span className='uk-text-truncate'>{user.get('title')}</span>
+                    <span className="uk-text-truncate">{user.get('title')}</span>
                   </h3>
                 </div>
               }
               content={
-                <ul className='tru-list'>
+                <ul className="tru-list">
                   <li>
-                    <div className='tru-list-content'>
-                      <span className='tru-list-heading'>Role</span>
-                      <span className='uk-text-small uk-text-muted'>{user.getIn(['role', 'name'])}</span>
+                    <div className="tru-list-content">
+                      <span className="tru-list-heading">Role</span>
+                      <span className="uk-text-small uk-text-muted">{user.getIn(['role', 'name'])}</span>
                     </div>
                   </li>
                   <li>
-                    <div className='tru-list-content'>
-                      <span className='tru-list-heading'>Email</span>
-                      <span className='uk-text-small uk-text-muted'>
+                    <div className="tru-list-content">
+                      <span className="tru-list-heading">Email</span>
+                      <span className="uk-text-small uk-text-muted">
                         <a href={`mailto:${user.get('email')}`}>{user.get('email')}</a>
                       </span>
                     </div>
                   </li>
                   <li>
-                    <div className='tru-list-content'>
-                      <span className='tru-list-heading'>Phone</span>
-                      <span className='uk-text-small uk-text-muted'> {user.get('phone')} </span>
+                    <div className="tru-list-content">
+                      <span className="tru-list-heading">Phone</span>
+                      <a href={`mailto:${user.get('phone')}`}>{user.get('phone')}</a>
                     </div>
                   </li>
                   <li>
                     {customer && user.get('groups') && (
-                      <div className='tru-list-content'>
-                        <span className='tru-list-heading'>Groups</span>
-                        <span className='uk-text-small uk-text-muted uk-text-truncate'>
-                          {user.get('groups').map(group => {
-                            return group.get('name') + (user.get('groups').toArray().length > 1 ? ', ' : '')
+                      <div className="tru-list-content">
+                        <span className="tru-list-heading">Groups</span>
+                        <span className="uk-text-small uk-text-muted uk-text-truncate">
+                          {user.get('groups').map((group) => {
+                            return group.get('name') + (user.get('groups').toArray().length > 1 ? ', ' : '');
                           })}
                         </span>
                       </div>
                     )}
                     {!customer && user.get('teams') && (
-                      <div className='tru-list-content'>
-                        <span className='tru-list-heading'>Teams</span>
-                        <span className='uk-text-small uk-text-muted uk-text-truncate'>
-                          {user.get('teams').map(team => {
-                            return team.get('name') + (user.get('teams').toArray().length > 1 ? ', ' : '')
+                      <div className="tru-list-content">
+                        <span className="tru-list-heading">Teams</span>
+                        <span className="uk-text-small uk-text-muted uk-text-truncate">
+                          {user.get('teams').map((team) => {
+                            return team.get('name') + (user.get('teams').toArray().length > 1 ? ', ' : '');
                           })}
                         </span>
                       </div>
@@ -207,11 +209,11 @@ class AccountsContainer extends React.Component {
                   </li>
                   {!customer && user.get('departments') && (
                     <li>
-                      <div className='tru-list-content'>
-                        <span className='tru-list-heading'>Departments</span>
-                        <span className='uk-text-small uk-text-muted uk-text-truncate'>
-                          {user.get('departments').map(department => {
-                            return department.get('name') + (user.get('departments').toArray().length > 1 ? ', ' : '')
+                      <div className="tru-list-content">
+                        <span className="tru-list-heading">Departments</span>
+                        <span className="uk-text-small uk-text-muted uk-text-truncate">
+                          {user.get('departments').map((department) => {
+                            return department.get('name') + (user.get('departments').toArray().length > 1 ? ', ' : '');
                           })}
                         </span>
                       </div>
@@ -221,8 +223,8 @@ class AccountsContainer extends React.Component {
               }
             />
           </GridItem>
-        )
-      })
+        );
+      });
 
     return (
       <div>
@@ -288,7 +290,7 @@ class AccountsContainer extends React.Component {
           </InfiniteScroll>
         </PageContent>
       </div>
-    )
+    );
   }
 }
 
@@ -302,20 +304,20 @@ AccountsContainer.propTypes = {
   showModal: PropTypes.func.isRequired,
   common: PropTypes.object.isRequired,
   shared: PropTypes.object.isRequired,
-  accountsState: PropTypes.object.isRequired
-}
+  accountsState: PropTypes.object.isRequired,
+};
 
 AccountsContainer.defaultProps = {
   title: 'Accounts',
-  view: 'customers'
-}
+  view: 'customers',
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   shared: state.shared,
   accountsState: state.accountsState,
-  common: state.common
-})
+  common: state.common,
+});
 
 export default connect(mapStateToProps, { fetchAccounts, deleteAccount, enableAccount, unloadAccounts, showModal })(
   AccountsContainer
-)
+);
