c36d1b0ffc247706b4e7c9b451eb0fa5e3347ba0
Alexandder Novozhenin <novozhenina@gmail.com>
2023-03-02 14:59:19 +0000

    Isuue-11 70

--- src/client/actions/blacklist.js
+++ src/client/actions/blacklist.js
@@ -1,5 +1,9 @@
 import { createAction } from 'redux-actions';
 import { FETCH_BLACKLIST, ADD_EMAIL } from 'actions/types';
 
-export const fetchBlackList = createAction(FETCH_BLACKLIST.ACTION);
+export const fetchBlackList = createAction(
+  FETCH_BLACKLIST.ACTION,
+  (payload) => payload,
+  () => ({ thunk: true })
+);
 export const addEmail = createAction(ADD_EMAIL.ACTION);
