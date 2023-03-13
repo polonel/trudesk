1771313c798ddd6a7a5414cd3314a3fac8e8c615
Alexandder Novozhenin <novozhenina@gmail.com>
2023-03-13 08:10:42 +0000

    Isuue-11 80

--- webpack.config.js
+++ webpack.config.js
@@ -141,6 +141,22 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
     ],
   },
   optimization: {
