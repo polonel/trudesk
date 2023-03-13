74537ad606ee05aee78d231c7a81245d0c099e71
Alexandder Novozhenin <novozhenina@gmail.com>
2023-03-13 07:52:45 +0000

    Isuue-11 80

--- src/client/containers/Settings/Legal/index.jsx
+++ src/client/containers/Settings/Legal/index.jsx
@@ -12,80 +12,80 @@
  *  Copyright (c) 2014-2019. All rights reserved.
  */
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Button from 'components/Button'
-import EasyMDE from 'components/EasyMDE'
-import { updateSetting } from 'actions/settings'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Button from 'components/Button';
+import EasyMDE from 'components/EasyMDE';
+import { updateSetting } from 'actions/settings';
 
-import helpers from 'lib/helpers'
-import SettingItem from 'components/Settings/SettingItem'
+import helpers from 'lib/helpers';
+import SettingItem from 'components/Settings/SettingItem';
 
 class LegalSettingsContainer extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      privacyPolicy: ''
-    }
+      privacyPolicy: '',
+    };
   }
 
-  getSetting (name) {
+  getSetting(name) {
     return this.props.settings.getIn(['settings', name, 'value'])
       ? this.props.settings.getIn(['settings', name, 'value'])
-      : ''
+      : '';
   }
 
-  onSavePrivacyPolicyClicked (e) {
-    e.preventDefault()
-    console.log(this.state.privacyPolicy)
+  onSavePrivacyPolicyClicked(e) {
+    e.preventDefault();
+    console.log(this.state.privacyPolicy);
     this.props
       .updateSetting({
         stateName: 'privacyPolicy',
         name: 'legal:privacypolicy',
         value: this.state.privacyPolicy,
-        noSnackbar: true
+        noSnackbar: true,
       })
       .then(() => {
-        helpers.UI.showSnackbar('Privacy Policy Updated')
-      })
+        helpers.UI.showSnackbar('Privacy Policy Updated');
+      });
   }
 
-  render () {
-    const { active } = this.props
+  render() {
+    const { active } = this.props;
     return (
       <div className={!active ? 'hide' : ''}>
         <SettingItem title={'Privacy Policy'} subtitle={'Paste in HTML/Text of your privacy policy.'}>
           <div>
             <EasyMDE
               defaultValue={this.getSetting('privacyPolicy')}
-              onChange={v => this.setState({ privacyPolicy: v })}
+              onChange={(v) => this.setState({ privacyPolicy: v })}
             />
           </div>
-          <div className='uk-clearfix'>
+          <div className="uk-clearfix">
             <Button
               text={'Save'}
               extraClass={'uk-float-right'}
               flat={true}
               style={'success'}
               waves={true}
-              onClick={e => this.onSavePrivacyPolicyClicked(e)}
+              onClick={(e) => this.onSavePrivacyPolicyClicked(e)}
             />
           </div>
         </SettingItem>
       </div>
-    )
+    );
   }
 }
 
 LegalSettingsContainer.propTypes = {
   active: PropTypes.bool,
   settings: PropTypes.object.isRequired,
-  updateSetting: PropTypes.func.isRequired
-}
+  updateSetting: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-  settings: state.settings.settings
-})
+const mapStateToProps = (state) => ({
+  settings: state.settings.settings,
+});
 
-export default connect(mapStateToProps, { updateSetting })(LegalSettingsContainer)
+export default connect(mapStateToProps, { updateSetting })(LegalSettingsContainer);
